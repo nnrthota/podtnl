c2ec1e2342988033252b523a29df5de28d8b12d6
Narendranath Reddy <narendranathreddythota@gmail.com>
2020-05-25 16:43:37 +0400

    Added brew commands

--- podtnl.rb
+++ podtnl.rb
@@ -1,5 +1,5 @@
 class Podtnl < Formula
-  desc "A Powerful CLI that makes your pod available to online without exposing a k8 service"
+  desc "Powerful CLI that makes your pod available to online"
   homepage "https://narendranathreddythota.github.io/podtnl/"
   url "https://github.com/narendranathreddythota/podtnl/archive/1.0.tar.gz"
   sha256 "e94d28fae97ee38ee3f4b3a502df5c510280eb49868aeb2cad40a7266158c0cc"
@@ -8,6 +8,6 @@ class Podtnl < Formula
     system "go", "build", *std_go_args
   end
   test do
-   system "#{bin}/podtnl", "-v"
+    system "#{bin}/podtnl", "-v"
   end
 end
