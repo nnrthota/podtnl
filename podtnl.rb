2ef78fd2d84e3230e7ecb5f28d6a8a6d5eba4c98
Narendranath Reddy <narendranathreddythota@gmail.com>
2020-05-25 13:09:24 +0400

    updated podtnl.rb

--- podtnl.rb
+++ podtnl.rb
@@ -1,19 +1,13 @@
 class Podtnl < Formula
-  desc "Expose your pod to Online from any kubernetes clusters"
-  homepage "https://podtnl.sh"
+  desc "A Powerful CLI that makes your pod available to online without exposing a k8 service"
+  homepage "https://narendranathreddythota.github.io/podtnl/"
   url "https://github.com/narendranathreddythota/podtnl/archive/1.0.tar.gz"
   sha256 "e94d28fae97ee38ee3f4b3a502df5c510280eb49868aeb2cad40a7266158c0cc"
-  head "https://github.com/narendranathreddythota/podtnl.git"
   depends_on "go" => :build
   def install
-    ENV["GOPATH"] = buildpath
-    path = buildpath/"src/github.com/narendranathreddythota/podtnl"
-    path.install Dir["*"]
-    cd path do
-      system "go", "build", "-o", "#{bin}/podtnl"
-    end
+    system "go", "build", *std_go_args
   end
   test do
-    system "#{bin}/podtnl", "-v"
+   system "#{bin}/podtnl", "-v"
   end
 end
